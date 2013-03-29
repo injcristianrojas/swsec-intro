dda69ace8ede63aaabccc85bd1c86c3cbf1a0e47
Cristián Rojas <injcristianrojas@gmail.com>
2013-03-29 19:59:31 -0300

    Pruebas de integración usando JWebUnit ahora

--- src/test/java/injcristianrojas/MainITest.java
+++ src/test/java/injcristianrojas/MainITest.java
@@ -1,56 +1,33 @@
 package injcristianrojas;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
+import net.sourceforge.jwebunit.junit.WebTester;
 
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-
 public class MainITest {
 
-	private String startUrl = "http://127.0.0.1:8080/login.jsp";
-	private WebClient webClient;
+	private WebTester tester;
 
 	@Before
-	public void setUp() throws Exception {
-		webClient = new WebClient();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		webClient.closeAllWindows();
+	public void prepare() {
+		tester = new WebTester();
+		tester.setBaseUrl("http://127.0.0.1:8080/");
 	}
 
 	@Test
-	public void test() throws FailingHttpStatusCodeException,
-			MalformedURLException, IOException {
-		System.out.println("Probando web en " + startUrl);
-		HtmlPage page1 = webClient.getPage(startUrl);
-		Assert.assertEquals("Fans de los bellos paisajes (SWSEC Intro)", page1.getTitleText());
-
-		HtmlForm form = page1.getFormByName("mainForm");
-		HtmlTextInput usernameField = form.getInputByName("username");
-		HtmlPasswordInput passwordField = form.getInputByName("password");
-		HtmlSubmitInput submitButton = form.getInputByName("submitButton");
-		usernameField.setValueAttribute("crirojas");
-		passwordField.setValueAttribute("123");
-
-		HtmlPage page2 = submitButton.click();
-		Assert.assertTrue(page2.asXml().contains("Usuario: crirojas"));
-
-		HtmlPage page3 = page2.getHtmlElementById("exit").click();
-		Assert.assertTrue(!page3.asXml().contains("Usuario: crirojas"));
-		Assert.assertTrue(page3.asXml().contains("Fans de los bellos paisajes"));
+	public void test() {
+		tester.beginAt("login.jsp");
+		tester.assertTitleEquals("Fans de los bellos paisajes (SWSEC Intro)");
+		tester.setTextField("username", "crirojas");
+		tester.setTextField("password", "123");
+		tester.submit();
+		
+		tester.assertTextPresent("Usuario: crirojas");
+		tester.clickLink("exit");
+		
+		tester.assertTextNotPresent("Usuario: crirojas");
+		tester.assertTextPresent("Muro de visitas");
 	}
 
 }
