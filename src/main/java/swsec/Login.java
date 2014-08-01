7833b3287a84dfbead8c30ad95ebac5e63a622a4
Cristián Rojas <injcristianrojas@gmail.com>
2014-08-01 14:31:14 -0400

    Corrección de indentado en Login

--- src/main/java/swsec/Login.java
+++ src/main/java/swsec/Login.java
@@ -14,43 +14,43 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Login extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-		private ServletContext servletcontext;
+	private static final long serialVersionUID = 1L;
+	private ServletContext servletcontext;
 
-		public Login() {
-			super();
-		}
+	public Login() {
+		super();
+	}
 
-		public void init(ServletConfig config) throws ServletException {
-			servletcontext = config.getServletContext();
-		}
+	public void init(ServletConfig config) throws ServletException {
+		servletcontext = config.getServletContext();
+	}
 
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			HttpSession session = request.getSession(true);
-			String username = request.getParameter("username");
-			String password = request.getParameter("password");
-			try {
-				Class.forName("org.sqlite.JDBC");
-				Connection conexion = DriverManager.getConnection(Config.getSqliteUrl(servletcontext));
-				Statement statement = conexion.createStatement();
-				String query = "select * from usuarios where username='" + username + "' and password = '" + password + "'";
-				ResultSet resultado = statement.executeQuery(query);
-				int numFilas = 0;
-				while (resultado.next())
-					numFilas++;
-				statement.close();
-				conexion.close();
-				if (numFilas > 0) {
-					session.setAttribute("userIP", request.getRemoteAddr());
-					session.setAttribute("userHost", request.getRemoteHost());
-					session.setAttribute("username", username);
-					response.sendRedirect("saludos.jsp");
-				} else {
-					response.sendRedirect("login.jsp?notFoundError=1");
-				}
-			} catch (Exception e) {
-				throw new ServletException(e);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conexion = DriverManager.getConnection(Config.getSqliteUrl(servletcontext));
+			Statement statement = conexion.createStatement();
+			String query = "select * from usuarios where username='" + username + "' and password = '" + password + "'";
+			ResultSet resultado = statement.executeQuery(query);
+			int numFilas = 0;
+			while (resultado.next())
+				numFilas++;
+			statement.close();
+			conexion.close();
+			if (numFilas > 0) {
+				session.setAttribute("userIP", request.getRemoteAddr());
+				session.setAttribute("userHost", request.getRemoteHost());
+				session.setAttribute("username", username);
+				response.sendRedirect("saludos.jsp");
+			} else {
+				response.sendRedirect("login.jsp?notFoundError=1");
 			}
+		} catch (Exception e) {
+			throw new ServletException(e);
 		}
+	}
 
 }
