d9171c8792669c615d65547e22471d6e788922ef
Cristián Rojas <injcristianrojas@gmail.com>
2015-04-16 19:27:29 -0300

    Corregido problema de creación de DB vacía.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM maven:3.2-jdk-7-onbuild
 MAINTAINER Cristián Rojas
 
 RUN apt-get update
-RUN apt-get install -y nano vim emacs24-nox
+RUN apt-get install -y nano vim emacs24-nox sqlite3
 
 VOLUME /usr/src/app
 
--- src/main/java/swsec/dbsetup/DBSetup.java
+++ src/main/java/swsec/dbsetup/DBSetup.java
@@ -11,7 +11,7 @@ public class DBSetup {
 
 	private static final String DB_FILE = "src/main/webapp/WEB-INF/swsecdemo.sqlite";
 
-	public static void main(String[] args) throws SqlJetException {
+	public static void main(String[] args) {
 
 		// creamos la db desde cero
 		File dbFile = new File(DB_FILE);
@@ -21,33 +21,30 @@ public class DBSetup {
 				return;
 			}
 		}
-		System.out.println("Creando base de datos");
-		SqlJetDb db = SqlJetDb.open(dbFile, true);
-		db.getOptions().setAutovacuum(true);
-		db.beginTransaction(SqlJetTransactionMode.WRITE);
-		try {
-			db.createTable("CREATE TABLE 'mensajes' ('id' INTEGER PRIMARY KEY NOT NULL , 'mensaje' TEXT NOT NULL );");
-			db.createTable("CREATE TABLE 'usuarios' ('id' INTEGER PRIMARY KEY NOT NULL , 'username' VARCHAR(10) NOT NULL , 'password' VARCHAR(10) NOT NULL , 'type' INTEGER);");
-		} finally {
-			db.commit();
-		}
-
-		db.beginTransaction(SqlJetTransactionMode.WRITE);
-		try {
-			ISqlJetTable table = db.getTable("usuarios");
-			table.insert("jperez", "123", "1");
-			table.insert("basaber", "12345", "1");
-			table.insert("skramer", "power", "2");
-			table.insert("aeinstein", "simple", "2");
-			table.insert("tstark", "ironman", "2");
-			table.insert("cpalma", "lepego", "2");
-			table.insert("asavage", "boom", "1");
-			table.insert("jhyneman", "boom", "1");
-			table.insert("tanderson", "matrix", "1");
-			table.insert("zcool", "god", "1");
-		} finally {
-			db.commit();
-		}
-		db.close();
+		System.out.println("[INFO] ");
+		System.out.println("[INFO] Creando base de datos...");
+    SqlJetDb db = null;
+    try {
+      db = SqlJetDb.open(dbFile, true);
+      db.getOptions().setAutovacuum(true);
+      db.beginTransaction(SqlJetTransactionMode.WRITE);
+      db.createTable("CREATE TABLE 'mensajes' ('id' INTEGER PRIMARY KEY NOT NULL, 'mensaje' TEXT NOT NULL );");
+      db.createTable("CREATE TABLE 'usuarios' ('id' INTEGER PRIMARY KEY NOT NULL, 'username' VARCHAR(10) NOT NULL, 'password' VARCHAR(10) NOT NULL ,'type' INTEGER);");
+      ISqlJetTable table = db.getTable("usuarios");
+      table.insert("jperez", "123", "1");
+      table.insert("basaber", "12345", "1");
+      table.insert("skramer", "power", "2");
+      table.insert("aeinstein", "simple", "2");
+      table.insert("tstark", "ironman", "2");
+      table.insert("cpalma", "lepego", "2");
+      table.insert("asavage", "boom", "1");
+      table.insert("jhyneman", "boom", "1");
+      table.insert("tanderson", "matrix", "1");
+      table.insert("zcool", "god", "1");
+      db.commit();
+      db.close();
+    } catch (SqlJetException e) {
+      e.printStackTrace();
+    }
 	}
 }
