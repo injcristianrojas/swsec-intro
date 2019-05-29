6e714b8863f0e79e980852f70324a99b5ad79c09
Cristián Rojas <injcristianrojas@gmail.com>
2019-05-28 23:49:34 -0400

    Corrected test issues and refactoring

--- src/test/java/swsec/RestApiITest.java
+++ src/test/java/swsec/RestApiITest.java
@@ -13,6 +13,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.junit.Before;
 import org.junit.Test;
 
 import swsec.api.helpers.Constants;
@@ -22,6 +23,7 @@ public class RestApiITest {
 	private static String EXAMPLE_MESSAGE = "Bienvenidos a Fans de las Aves Chilenas. Soy el administrador.";
 	private String jwtToken;
 	
+	@Before
 	public void setUp() throws ClientProtocolException, IOException {
 		jwtToken = Constants.USE_JWT_AUTH ? getJWTToken() : null;
 	}
@@ -29,7 +31,7 @@ public class RestApiITest {
 	private String getJWTToken() throws ClientProtocolException, IOException {
 		HttpPost request = new HttpPost("http://127.0.0.1:8080/api/auth/login");
 		StringEntity rawData = new StringEntity("{ \"username\": \"" + TestConfig.DEFAULT_USER + "\", \"password\": \"" + TestConfig.DEFAULT_PASSWORD + "\"}");
-		request.addHeader("content-type", "application/json");
+		request.addHeader("Content-Type", "application/json");
 		request.setEntity(rawData);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
 		String token = response.getFirstHeader("Authorization").getValue();
@@ -39,7 +41,7 @@ public class RestApiITest {
 	@Test
 	public void checkUser() throws ClientProtocolException, IOException {
 		HttpGet request = new HttpGet("http://127.0.0.1:8080/api/users/get/1");
-		request.setHeader("Authorization", jwtToken);
+		if (jwtToken != null) request.addHeader("Authorization", jwtToken);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
 		String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
 		assertTrue(responseString.contains("jperez"));
@@ -48,9 +50,9 @@ public class RestApiITest {
 	@Test
 	public void testPost() throws ClientProtocolException, IOException {
 		HttpPost request = new HttpPost("http://127.0.0.1:8080/api/posts/add");
-		request.setHeader("Authorization", jwtToken);
+		if (jwtToken != null) request.addHeader("Authorization", jwtToken);
 		StringEntity rawData = new StringEntity("{ \"message\": \"" + EXAMPLE_MESSAGE+ "\" }");
-		request.addHeader("content-type", "application/json");
+		request.addHeader("Content-Type", "application/json");
 		request.setEntity(rawData);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
 		assertTrue(response.getStatusLine().getStatusCode() == Response.Status.OK.getStatusCode());
@@ -59,7 +61,7 @@ public class RestApiITest {
 	@Test
 	public void testIsPostCreated() throws ClientProtocolException, IOException {
 		HttpGet request = new HttpGet("http://127.0.0.1:8080/api/posts/get");
-		request.setHeader("Authorization", jwtToken);
+		if (jwtToken != null) request.setHeader("Authorization", jwtToken);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
 		String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
 		assertTrue(responseString.contains(EXAMPLE_MESSAGE));
