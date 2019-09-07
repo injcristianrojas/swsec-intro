272f44f353b48dbc7d891a17df5f5a151e4f7ab8
Cristián Rojas <injcristianrojas@gmail.com>
2019-09-07 16:21:37 -0400

    Back to 127.0.0.1

--- src/test/java/swsec/TestConfig.java
+++ src/test/java/swsec/TestConfig.java
@@ -3,5 +3,5 @@ package swsec;
 public class TestConfig {
 	public static String DEFAULT_USER = "jperez";
 	public static String DEFAULT_PASSWORD = "123";
-	public static String DEFAULT_HOST = "http://localhost.localdomain:8090";
+	public static String DEFAULT_HOST = "http://127.0.0.1:8090";
 }
