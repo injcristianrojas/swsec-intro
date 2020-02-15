e46ac3f829573f2790f9ed08faad7287fcf44299
Cristian Rojas <injcristianrojas@gmail.com>
2020-02-15 18:58:09 -0300

    Angular play

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Fans de las Aves Chilenas';
+  title = 'angular-app';
+
+  todoSubmit(value:any){
+    console.log(value)
+  }
+
 }
