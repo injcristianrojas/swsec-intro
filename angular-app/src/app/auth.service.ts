e84b5d48d8f1264c32f598f01b233eed06c4811a
Cristian Rojas <injcristianrojas@gmail.com>
2020-02-22 18:29:05 -0300

    JWT decoding

--- angular-app/src/app/auth.service.ts
+++ angular-app/src/app/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  expiration_time: number;
+
   constructor() { }
 
   setToken(token: string) {
+    const tokenpayload = jwt_decode(token);
+    this.expiration_time = tokenpayload.exp;
     localStorage.setItem('jwtToken', token);
   }
 
@@ -20,6 +25,9 @@ export class AuthService {
   }
 
   isSessionOpen(): boolean {
+    const current_time = Date.now() / 1000
+    if (this.expiration_time < current_time)
+      this.removeToken();
     return this.getToken() !== null;
   }
 
