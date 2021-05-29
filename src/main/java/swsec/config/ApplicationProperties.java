3eee4dcc5d13c32ae7f4ee512ad67cfd55be23d0
Cristian Rojas <injcristianrojas@gmail.com>
2021-05-28 21:31:49 -0400

    Tabs change

--- src/main/java/swsec/config/ApplicationProperties.java
+++ src/main/java/swsec/config/ApplicationProperties.java
@@ -8,32 +8,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public enum ApplicationProperties {
-    INSTANCE;
+  INSTANCE;
 
-    private final Properties properties;
+  private final Properties properties;
 
-    ApplicationProperties() {
-        properties = new Properties();
-        try {
-            properties.load(new FileInputStream("src/main/resources/app.properties"));
-        } catch (IOException e) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
-        }
+  ApplicationProperties() {
+    properties = new Properties();
+    try {
+      properties.load(new FileInputStream("src/main/resources/app.properties"));
+    } catch (IOException e) {
+      Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
     }
+  }
 
-    public String getProperties() {
-        return properties.toString();
-    }
+  public String getProperties() {
+    return properties.toString();
+  }
 
-    public boolean usesJWT() {
-        return properties.getProperty("jwt.enabled").equals("true");
-    }
+  public boolean usesJWT() {
+    return properties.getProperty("jwt.enabled").equals("true");
+  }
 
-    public String testUser() {
-        return properties.getProperty("test.default_user");
-    }
+  public String testUser() {
+    return properties.getProperty("test.default_user");
+  }
 
-    public String testPassword() {
-        return properties.getProperty("test.default_password");
-    }
+  public String testPassword() {
+    return properties.getProperty("test.default_password");
+  }
 }
--- src/main/java/swsec/config/DatabasePopulater.java
+++ src/main/java/swsec/config/DatabasePopulater.java
@@ -13,43 +13,45 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabasePopulater implements ServletContextListener {
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        Logger.getLogger(getClass().getName()).log(Level.INFO, "JWT is " + (ApplicationProperties.INSTANCE.usesJWT() ? "enabled" : "disabled"));
-        populateDatabase();
-    }
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    Logger.getLogger(getClass().getName()).log(Level.INFO,
+        "JWT is " + (ApplicationProperties.INSTANCE.usesJWT() ? "enabled" : "disabled"));
+    populateDatabase();
+  }
 
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
 
-    }
+  }
 
-    private void populateDatabase() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String messageTable = "CREATE TABLE IF NOT EXISTS 'mensajes' ('id' INTEGER PRIMARY KEY NOT NULL, 'mensaje' TEXT NOT NULL );";
-            String userTable = "CREATE TABLE IF NOT EXISTS 'usuarios' ('id' INTEGER PRIMARY KEY NOT NULL, 'username' VARCHAR(10) NOT NULL, 'password' VARCHAR(10) NOT NULL ,'type' INTEGER);";
-            Connection conn = DriverManager.getConnection(Helpers.SQLITE_URL);
-            Statement stmt = conn.createStatement();
-            stmt.setQueryTimeout(30);
-            stmt.executeUpdate(userTable);
-            stmt.executeUpdate(messageTable);
-            stmt.executeUpdate("DELETE FROM usuarios");
-            stmt.executeUpdate("DELETE FROM mensajes");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('admin', 'admin', '1')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('jperez', '123', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('basaber', '12345', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('skramer', 'power', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('aeinstein', 'simple', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('tstark', 'ironman', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('cpalma', 'lepego', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('asavage', 'boom', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('jhyneman', 'boom', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('tanderson', 'matrix', '2')");
-            stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('zcool', 'god', '2')");
-            stmt.executeUpdate("INSERT into mensajes(mensaje) VALUES ('Bienvenidos al foro de Fans de las Aves Chilenas. Soy el Administrador.')");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  private void populateDatabase() {
+    try {
+      Class.forName("org.sqlite.JDBC");
+      String messageTable = "CREATE TABLE IF NOT EXISTS 'mensajes' ('id' INTEGER PRIMARY KEY NOT NULL, 'mensaje' TEXT NOT NULL );";
+      String userTable = "CREATE TABLE IF NOT EXISTS 'usuarios' ('id' INTEGER PRIMARY KEY NOT NULL, 'username' VARCHAR(10) NOT NULL, 'password' VARCHAR(10) NOT NULL ,'type' INTEGER);";
+      Connection conn = DriverManager.getConnection(Helpers.SQLITE_URL);
+      Statement stmt = conn.createStatement();
+      stmt.setQueryTimeout(30);
+      stmt.executeUpdate(userTable);
+      stmt.executeUpdate(messageTable);
+      stmt.executeUpdate("DELETE FROM usuarios");
+      stmt.executeUpdate("DELETE FROM mensajes");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('admin', 'admin', '1')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('jperez', '123', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('basaber', '12345', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('skramer', 'power', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('aeinstein', 'simple', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('tstark', 'ironman', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('cpalma', 'lepego', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('asavage', 'boom', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('jhyneman', 'boom', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('tanderson', 'matrix', '2')");
+      stmt.executeUpdate("INSERT INTO usuarios(username, password, type) VALUES ('zcool', 'god', '2')");
+      stmt.executeUpdate(
+          "INSERT into mensajes(mensaje) VALUES ('Bienvenidos al foro de Fans de las Aves Chilenas. Soy el Administrador.')");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
